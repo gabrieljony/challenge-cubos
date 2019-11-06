33a8d1459f44a4e50fa9f64d409cfe08085c82f8
gabriel jony <gabriel@polisystem.com.br>
2019-11-06 17:36:41 -0300

    Adicionado a Api na aplicação themoviedb

---
+++ src/services/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const urlAPI = "https://api.themoviedb.org/3/";
+const apikey = "67f2745bfbab836b3214ba02776d32ba";
+const language = "language=pt-br";
+
+const api = axios.create({
+    baseURL: urlAPI
+});
+
+export default api;
+
+// Buscar pelo nome do filme
+export const movies = {
+    options: {
+        url: `search/movie?api_key=${apikey}&${language}&query=`
+    }
+};
+
+// Buscar filme pelo seu id passado como parametro
+export const movieId = id => ({
+    options: {
+        url: `movie/${id}?api_key=${apikey}&${language}&append_to_response=videos`
+    }
+});
+
+// Buscar filme por genero
+export const genres = {
+    options: {
+        url: `genre/movie/list?api_key=${apikey}&${language}`
+    }
+};
+
+// Buscar filme por ano
